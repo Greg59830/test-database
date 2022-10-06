10f46495af376bb652838863b15a6773aa6ebd04
Greg59830 <108268555+Greg59830@users.noreply.github.com>
2022-10-06 14:54:01 +0200

    Add files via upload

---
+++ 5 worst sales 2022.sql
@@ -0,0 +1,9 @@
+use toys_and_models;
+-- nombre produit le + commandé l'annee en cours
+SELECT year(orderDate) as year, sum(quantityOrdered) as nb_produit_commandé, products.productName AS ProdName
+From orders
+LEFT JOIN orderdetails USING(orderNumber)
+LEFT JOIN products USING(productCode)
+WHERE year(orderDate) = year(now())
+GROUP BY productCode, year, ProdName
+Order by nb_produit_commandé ASC limit 5;
